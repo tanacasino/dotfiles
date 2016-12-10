b038f64418af0042e477c90dd20e4fd9352b343c
Tomofumi Tanaka <tanacasino@gmail.com>
2016-12-10 22:57:49 +0900

    Remove prompt config

--- fish/config.fish
+++ fish/config.fish
@@ -6,80 +6,31 @@
 ###########
 # Alias   #
 ###########
+# git
 alias g='git'
+alias gsb='git status -sb'
 alias gst='git status'
+alias gd='git diff'
+alias gf='git fetch'
+alias ggpush='git push origin (git_current_branch)'
+alias gb='git branch'
+alias gc='git commit -v'
+alias 'gc!'='git commit -v --amend'
+alias gco='git checkout'
+alias glg='git log --stat --color'
+alias glgg='git log --graph --color'
 
+# tmux
+alias tmux='env TERM=xterm-256color tmux'
+alias t=tmux
 
+# basic
+alias hist=history
+alias ls='gls --color=auto'
+alias l='ls -CF'
+alias la='ls -A'
+alias lh='ll -h'
+alias ll='ls -lF'
+alias lla='ls -AlF'
+alias vissh='vim $HOME/.ssh/config'
 
-###########
-# Prompt  #
-###########
-set __fish_git_prompt_color_branch blue
-
-set __fish_git_prompt_char_upstream_prefix ' '
-set __fish_git_prompt_char_stateseparator ' '
-
-set __fish_git_prompt_show_informative_status true
-set __fish_git_prompt_color_upstream_ahead yellow
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_color_upstream_behind yellow
-set __fish_git_prompt_char_upstream_behind '↓'
-
-
-
-set __fish_git_prompt_color_untrackedfiles red
-set __fish_git_prompt_char_untrackedfiles '*'
-
-set __fish_git_prompt_showdirtystate true
-set __fish_git_prompt_color_dirtystate red
-set __fish_git_prompt_char_dirtystate '±'
-
-set __fish_git_prompt_color_stagedstate yellow
-set __fish_git_prompt_char_stagedstate '⇈'
-
-set __fish_git_prompt_showuntrackedfiles true
-set __fish_git_prompt_color_cleanstate 777
-set __fish_git_prompt_char_cleanstate '✔'
-
-
-
-set _vbr_cyan    (set_color cyan)
-set _vbr_yellow  (set_color yellow)
-set _vbr_red     (set_color red)
-set _vbr_blue    (set_color blue)
-set _vbr_green   (set_color green)
-set _vbr_gray    (set_color 777)
-
-
-
-# [user@host] path [branch [git status]] prompt
-function fish_prompt
-    set -l exit_code $status
-
-    set -l prompt ''
-    set -e prompt[1]
-
-    # display username and hostname if logged in as root, in sudo or ssh session
-    if [ \( (id -u) -eq 0 -o $SUDO_USER \) -o $SSH_CONNECTION ]
-        set -l host (hostname | cut -f 1 -d '.')
-        set prompt $prompt $_vbr_yellow$USER$_vbr_gray'@'$_vbr_cyan$host
-    end
-
-    # path
-    set prompt $prompt $_vbr_gray(pwd | sed "s:^$HOME:~:")
-
-    # Git
-    set prompt $prompt (__fish_git_prompt '%s')
-
-    # show prompt and newline
-    echo -e $prompt
-
-    # prompt symbol (next line)
-    set -l prompt ''
-    set -e prompt[1]
-    if [ $exit_code != 0 ]; set prompt $prompt $_vbr_red"[$exit_code]"'$ '
-    else;                   set prompt $prompt $_vbr_green'$ '
-    end
-
-    echo -e $prompt (set_color normal)
-end
