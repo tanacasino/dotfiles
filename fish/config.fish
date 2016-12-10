23dd4724792dbc06ea060e3305ad8df08a14bea0
Tomofumi Tanaka <tanacasino@gmail.com>
2016-12-11 00:24:45 +0900

    Update fish shell config

--- fish/config.fish
+++ fish/config.fish
@@ -3,16 +3,47 @@
 # $HOME/.config/fish/config.fish
 #
 
-###########
-# Alias   #
-###########
+############
+# Variable #
+############
+set -U FZF_DEFAULT_OPTS "--reverse --inline-info"
+
+
+
+############
+# Bind     #
+############
+bind \cf forward-char
+
+
+
+############
+# Function #
+############
+# cd to ghq repository dir with fzf
+function __fzf_ghq_cd
+  set -q FZF_CD_COMMAND
+  or set -l FZF_CD_COMMAND "
+  command ghq list --full-path"
+  fish -c "$FZF_CD_COMMAND" | __fzfcmd -m $FZF_CD_OPTS | read -la select
+  if test ! (count $select) -eq 0
+    cd "$select"
+  end
+  commandline -f repaint
+end
+
+
+
+############
+# Alias    #
+############
 # git
 alias g='git'
 alias gsb='git status -sb'
 alias gst='git status'
 alias gd='git diff'
-alias gf='git fetch --prune --tags --verbose'
-alias gfa='git fetch --prune --tags --verbose --all'
+alias gf='git fetch --prune --verbose'
+alias gfa='git fetch --all --prune --tags --verbose'
 alias ggpush='git push origin (git_branch_name)'
 alias gb='git branch'
 alias gc='git commit -v'
@@ -20,6 +51,7 @@ alias 'gc!'='git commit -v --amend'
 alias gco='git checkout'
 alias glg='git log --stat --color'
 alias glgg='git log --graph --color'
+alias gcd='__fzf_ghq_cd'
 
 # tmux
 alias tmux='env TERM=xterm-256color tmux'
@@ -34,10 +66,4 @@ alias lh='ll -h'
 alias ll='ls -lF'
 alias lla='ls -AlF'
 alias vissh='vim $HOME/.ssh/config'
-
-
-###########
-# Bind    #
-###########
-bind \cf forward-char
 
