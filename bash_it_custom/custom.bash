1028a7bba73cd70ca19f60b7d6050eb67bc48796
Tomofumi Tanaka <tanacasino@gmail.com>
2017-03-15 23:22:23 +0900

    Add keybind

--- bash_it_custom/custom.bash
+++ bash_it_custom/custom.bash
@@ -2,8 +2,11 @@
 # Variables
 #############################
 # TODO(tanacasino): 優先順位を変えたいだけなので、重複を取り除いてソートするようにしたい
-export PATH="/usr/local/bin:$PATH"
-
+export PATH="$HOME/devel/bin:/usr/local/bin:$PATH"
+export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_DEFAULT_OPTS="--reverse --inline-info"
+export GOPATH=$HOME/devel
+export PATH="$GOPATH/bin:/usr/local/bin:$PATH"
 
 
 #############################
@@ -11,7 +14,6 @@ export PATH="/usr/local/bin:$PATH"
 #############################
 # Show current git branch name (or tag name)
 function git_branch_name {
-
     local branch_name="$(git symbolic-ref --short HEAD 2>/dev/null)"
 
     if [ -z "$branch_name" ]; then
@@ -24,15 +26,29 @@ function git_branch_name {
     else
         echo "$branch_name"
     fi
-
 }
+
+
+function ghq_cd_repository() {
+    local selected="$(ghq list --full-path | $(__fzfcmd) --query="$READLINE_LINE")"
+    if [ -n "$selected" ]; then
+        echo cd "$selected"
+    fi
+}
+
+bind '"\C-]": " \C-e\C-u`ghq_cd_repository`\e\C-e\e^\er"'
 
 
 
 #############################
 # Aliases
 #############################
+# basic
+alias vissh='vim $HOME/.ssh/config'
+alias lh='ll -h'
+alias lla='ls -AlF'
 
+# git
 alias ggpush='git push origin $(git_branch_name)'
 
 
